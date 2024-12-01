3c93ba6e75a6a4eb256d8001af97ce66e5a3bc5c
saichethanr <saichethan9019@gmail.com>
2024-12-01 17:05:39 +0530

    done

--- Arrays/checkarrsortedandrotated.java
+++ Arrays/checkarrsortedandrotated.java
@@ -1,6 +1,9 @@
 package Arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class checkarrsortedandrotated {
     //(96/109)
@@ -27,5 +30,26 @@ public class checkarrsortedandrotated {
             }
         }
         return true;
+    }
+    //solution
+    public boolean check_ans(int[] nums) {
+        List<Integer> arr = new ArrayList<>();
+        for (int num : nums) {
+            arr.add(num);
+        }
+
+        List<Integer> sorted = new ArrayList<>(arr);
+        Collections.sort(sorted);
+
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            if (arr.equals(sorted)) {
+                return true;
+            }
+            int num = arr.remove(size - 1);
+            arr.add(0, num);
+        }
+
+        return false;
     }
 }
